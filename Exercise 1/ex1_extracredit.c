d0923d4cbd0fae6bb6df56de0b43214ba0c9d1af
Joseph Murphy <jmurphy5277@gmail.com>
2015-02-22 20:06:09 -0500

    Learning C

---
+++ Exercise 1/ex1_extracredit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	puts("Hello world.");
+	puts("Line one");
+	puts("Line two");
+	puts("Line three");
+	puts("Line four");
+	puts("Line five");
+	return 0;
+
+}
+
---
+++ Exercise 6/ex6_emptystring.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+	char empty[] = "";
+	printf("This should be empty: %s.\n", empty);
+
+	return 0;
+}
